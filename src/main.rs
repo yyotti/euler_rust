2a1895d3790e25c4424d9b365c2c6d88b9a401c0
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2019-08-26 12:08:39 +0900

    実行する問題番号を指定できるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#[macro_use]
+extern crate clap;
+
 use euler_rust::*;
+use clap::Arg;
+use clap::ErrorKind;
 
 fn main() {
     let solvers: Vec<Box<Solver<_>>> = vec![
@@ -8,7 +13,25 @@ fn main() {
         Box::new(p0004::Solver),
     ];
 
-    for s in solvers {
-        println!("{}", s.solve());
-    }
+    let app = app_from_crate!()
+        .arg(
+            Arg::with_name("number")
+                .help("Problem number (positive integer). If omitted, show all answers.")
+                .short("n")
+                .long("num")
+                .multiple(true)
+                .takes_value(true),
+        );
+
+    let matches = app.get_matches();
+    match values_t!(matches, "number", usize) {
+        Ok(ref nums) if (|| nums.iter().all(|&n| n > 0 && n < solvers.len()))() => for n in nums {
+            println!("{}", solvers[n-1].solve());
+        },
+        Err(ref e) if e.kind == ErrorKind::ArgumentNotFound => {
+            solvers.iter().for_each(|s| println!("{}", s.solve()));
+        },
+        Err(e) => println!("{}", e),
+        _ => println!("{}", "Cannot ..."),
+    };
 }
