68b1053f9189138a71d3906d7ed3c7d0fb75ae64
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2019-09-04 16:02:03 +0900

    Problem 18 に解法を追加（ダイクストラ）

--- src/p0018.rs
+++ src/p0018.rs
@@ -1,5 +1,7 @@
 //! [Problem 18](https://projecteuler.net/problem=18)([JP](http://www.odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2018))
 
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
 use std::iter::repeat;
 
 pub struct Solver;
@@ -24,22 +26,46 @@ const NUMS: &[&[u64]] = &[
 
 impl super::Solver<u64> for Solver {
     fn solve(&self) -> u64 {
-        solve(NUMS, (0, 0))
+        solve(NUMS)
     }
 }
 
-fn solve(triangle: &[&[u64]], p: (usize, usize)) -> u64 {
-    // 真面目に全ルートを探索して最大を探す。
-    // 問題文にある通り、この問題だからまだ可能なやり方。
-    if p.0 > triangle.len() - 1 || p.1 > triangle[p.0].len() - 1 {
-        return 0;
-    }
+fn solve(triangle: &[&[u64]]) -> u64 {
+    // ダイクストラ法でやってみる。
+    // 頂点を始点、底辺の各マスを終点として、最もコストがかかる点を求めればよい。
+    // 今回は経路自体は問題にしていないので省略する。
+    let s = triangle[0][0];
 
-    vec![(p.0 + 1, p.1), (p.0 + 1, p.1 + 1)]
-        .iter()
-        .map(|&(x, y)| solve(triangle, (x, y)) + triangle[p.0][p.1])
+    let mut d = HashMap::new();
+    d.insert((0, 0), s);
+    let mut q = BinaryHeap::from(vec![(s, (0, 0))]);
+
+    while q.len() > 0 {
+        let (_, (x, y)) = q.pop().unwrap();
+        vec![(x + 1, y), (x + 1, y + 1)]
+            .iter()
+            .filter(|(i, j)| i < &triangle.len() && j < &triangle[*i].len())
+            .for_each(|&(i, j)| {
+                let alt = match d.get(&(x, y)) {
+                    Some(d) => d + triangle[i][j],
+                    _ => triangle[i][j],
+                };
+                if &alt > d.get(&(i, j)).unwrap_or(&0) {
+                    d.insert((i, j), alt);
+                    q.push((alt, (i, j)));
+                }
+            });
+    }
+    *d.iter()
+        .filter_map(|(k, v)| {
+            if k.0 == triangle.len() - 1 {
+                Some(v)
+            } else {
+                None
+            }
+        })
         .max()
-        .unwrap_or(0)
+        .unwrap()
 }
 
 #[allow(dead_code)]
@@ -60,6 +86,21 @@ fn solve2(triangle: &[&[u64]]) -> u64 {
     )[0]
 }
 
+#[allow(dead_code)]
+fn solve3(triangle: &[&[u64]], p: (usize, usize)) -> u64 {
+    // 真面目に全ルートを探索して最大を探す。
+    // 問題文にある通り、この問題だからまだ可能なやり方。
+    if p.0 > triangle.len() - 1 || p.1 > triangle[p.0].len() - 1 {
+        return 0;
+    }
+
+    vec![(p.0 + 1, p.1), (p.0 + 1, p.1 + 1)]
+        .iter()
+        .map(|&(x, y)| solve3(triangle, (x, y)) + triangle[p.0][p.1])
+        .max()
+        .unwrap_or(0)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -73,7 +114,8 @@ mod tests {
             &[8, 5, 9, 3], //
         ];
 
-        assert_eq!(23, solve(triangle, (0, 0)), "solve1");
+        assert_eq!(23, solve(triangle), "solve1");
         assert_eq!(23, solve2(triangle), "solve2");
+        assert_eq!(23, solve3(triangle, (0, 0)), "solve3");
     }
 }
