3b7bbb0a5e14492ac470f7065547c0d4d94e16a4
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2019-09-06 11:16:18 +0900

    Problem 21 を改善

--- src/p0021.rs
+++ src/p0021.rs
@@ -1,6 +1,7 @@
 //! [Problem 21](https://projecteuler.net/problem=21)([JP](http://www.odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2021))
 
 use super::common::prime_factors;
+use std::iter::repeat;
 
 pub struct Solver;
 
@@ -13,6 +14,32 @@ impl super::Solver<u64> for Solver {
 }
 
 fn solve(input: u64) -> u64 {
+    // いちいち素因数分解してやるのは時間がかかりそうなので、エラトステネス風
+    // に約数の和を計算してみる。
+    let m = input as usize;
+    let factors = (2..m).fold(repeat(1).take(m).collect::<Vec<usize>>(), |mut acc, i| {
+        (i + i..m).step_by(i).for_each(|j| acc[j] += i);
+        acc
+    });
+
+    (1..m)
+        .filter_map(|i| {
+            if i < 2 {
+                return None;
+            }
+
+            let k = &factors[i];
+            if k < &m && k != &i && factors[*k] == i {
+                Some(i as u64)
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+#[allow(dead_code)]
+fn solve2(input: u64) -> u64 {
     // 素直にやる
     (1..input).filter(|&n| has_amicable_number(n)).sum()
 }
@@ -24,7 +51,8 @@ fn has_amicable_number(n: u64) -> bool {
         return false;
     }
 
-    sum_factors(d - n) == d
+    let e = sum_factors(d - n);
+    e > d - n && e - (d - n) == n
 }
 
 fn sum_factors(n: u64) -> u64 {
@@ -56,11 +84,14 @@ mod tests {
         assert_eq!(false, has_amicable_number(100));
         assert_eq!(true, has_amicable_number(220));
         assert_eq!(true, has_amicable_number(284));
+        assert_eq!(true, has_amicable_number(1184));
+        assert_eq!(true, has_amicable_number(1210));
     }
 
     #[test]
     fn test_solve() {
         // 1000未満の友愛数は (220, 284) のみ
-        assert_eq!(504, solve(1000));
+        assert_eq!(504, solve(1000), "solve1");
+        assert_eq!(504, solve2(1000), "solve2");
     }
 }
