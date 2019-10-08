4162696ba9f18106abd1352c84eba9a23a8ae3e4
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2019-10-08 16:09:00 +0900

    Problem 31 を改善

--- src/p0031.rs
+++ src/p0031.rs
@@ -4,7 +4,7 @@ pub struct Solver;
 
 const N: usize = 200;
 
-const COINS: &[usize] = &[200, 100, 50, 20, 10, 5, 2, 1];
+const COINS: &[usize] = &[100, 200, 20, 10, 50, 5, 2, 1];
 
 impl super::Solver for Solver {
     fn solve(&self) -> i64 {
@@ -15,19 +15,13 @@ impl super::Solver for Solver {
 fn solve(input: usize, coins: &[usize]) -> i64 {
     if input == 0 {
         return 1;
+    } else if coins.len() == 0 {
+        return 0;
     }
 
-    let mut cs = coins.iter().skip_while(|&&c| c > input);
-
-    cs.next()
-        .map(|coin| {
-            let tail = cs.copied().collect::<Vec<usize>>();
-            (1..=input / coin)
-                .map(|i| solve(input - i * coin, &tail))
-                .sum::<i64>()
-                + solve(input, &tail)
-        })
-        .unwrap_or(0)
+    let mut cs = coins.to_vec();
+    let c = cs.remove(0);
+    (0..=input / c).map(|i| solve(input - i * c, &cs)).sum()
 }
 
 #[cfg(test)]
@@ -36,19 +30,19 @@ mod tests {
 
     #[test]
     fn test_solve() {
-        let coins = &[2, 1];
-        assert_eq!(1, solve(0, coins), "coins={:?}, input={}", coins, 0);
-        assert_eq!(1, solve(1, coins), "coins={:?}, input={}", coins, 1);
-        assert_eq!(2, solve(2, coins), "coins={:?}, input={}", coins, 2);
-        assert_eq!(2, solve(3, coins), "coins={:?}, input={}", coins, 3);
-        assert_eq!(3, solve(4, coins), "coins={:?}, input={}", coins, 4);
+        let coins = vec![1, 2];
+        assert_eq!(1, solve(0, &coins), "coins={:?}, input={}", coins, 0);
+        assert_eq!(1, solve(1, &coins), "coins={:?}, input={}", coins, 1);
+        assert_eq!(2, solve(2, &coins), "coins={:?}, input={}", coins, 2);
+        assert_eq!(2, solve(3, &coins), "coins={:?}, input={}", coins, 3);
+        assert_eq!(3, solve(4, &coins), "coins={:?}, input={}", coins, 4);
 
         assert_eq!(11, solve(10, COINS), "coins={:?}, input={}", COINS, 10);
 
-        let coins = &[2];
-        assert_eq!(1, solve(0, coins), "coins={:?}, input={}", coins, 0);
-        assert_eq!(0, solve(1, coins), "coins={:?}, input={}", coins, 1);
-        assert_eq!(1, solve(2, coins), "coins={:?}, input={}", coins, 2);
-        assert_eq!(0, solve(3, coins), "coins={:?}, input={}", coins, 3);
+        let coins = vec![2];
+        assert_eq!(1, solve(0, &coins), "coins={:?}, input={}", coins, 0);
+        assert_eq!(0, solve(1, &coins), "coins={:?}, input={}", coins, 1);
+        assert_eq!(1, solve(2, &coins), "coins={:?}, input={}", coins, 2);
+        assert_eq!(0, solve(3, &coins), "coins={:?}, input={}", coins, 3);
     }
 }
